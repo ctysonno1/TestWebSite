fe451694402cae010deabe8bc7baaec3fe16e15a
Cory Tyson <coryt@datarg.com>
2016-11-04 14:04:59 -0400

    Initial commit

---
+++ PublistTestWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PublistTestWeb.Startup))]
+namespace PublistTestWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
